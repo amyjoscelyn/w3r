22a9e1021ff1b03970ab3d4ab0b762dd14792888
amyjoscelyn <amy.joscelyn@flatironschool.com>
2016-07-22 15:52:34 -0400

    Made new subclass of UIView, CardCluster

---
+++ Wither/Wither/CardCluster.swift
@@ -0,0 +1,14 @@
+//
+//  CardCluster.swift
+//  Wither
+//
+//  Created by Amy Joscelyn on 7/22/16.
+//  Copyright © 2016 Amy Joscelyn. All rights reserved.
+//
+
+import UIKit
+
+class CardCluster: UIView
+{
+    @IBOutlet private var contentView: UIView?
+}
